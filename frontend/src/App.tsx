c8c0f4cd6b63c9666d7305a17f27c4e8cab4f717
ThomasParratt <thomasparratt@googlemail.com>
2026-03-20 16:24:59 +0200

    react frontend added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState<{ id: number; name: string }[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
