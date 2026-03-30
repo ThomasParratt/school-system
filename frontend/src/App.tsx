94a5f0f8ce884e1b06c046a21f4c305088ad40e3
ThomasParratt <thomasparratt@googlemail.com>
2026-03-30 20:40:18 +0300

    react router working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Login from './components/Login'
+import Home from './components/Home'
 import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
