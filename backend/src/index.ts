d681690732bc0c31b317417834c4eccd4fd20cc6
ThomasParratt <thomasparratt@googlemail.com>
2026-05-18 19:13:22 +0300

    removed enrollments from index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,6 @@ import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/users.js";
 import courseRouter from "./routes/courses.js";
 import sessionRouter from "./routes/sessions.js";
-import enrollmentRouter from "./routes/enrollments.js";
 
 
 const app = express();
@@ -17,7 +16,6 @@ app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/courses", courseRouter);
 app.use("/sessions", sessionRouter);
-app.use("/enrollments", enrollmentRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
