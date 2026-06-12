e080358ee307be7d114d2abc3946c0b9a87619c8
ThomasParratt <thomasparratt@googlemail.com>
2026-06-12 12:47:33 +0300

    removed playground styling

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -92,6 +92,7 @@ export default function Calendar() {
             center: 'title',
             right: 'dayGridMonth,timeGridWeek,timeGridDay'
           }}
+          height="auto"
           initialView='dayGridMonth'
           editable={true}
           selectable={true}
@@ -126,14 +127,6 @@ function renderEventContent(eventInfo) {
 function Sidebar({ weekendsVisible, handleWeekendsToggle, currentEvents }) {
   return (
     <div className='demo-app-sidebar'>
-      <div className='demo-app-sidebar-section'>
-        <h2>Instructions</h2>
-        <ul>
-          <li>Select dates and you will be prompted to create a new event</li>
-          <li>Drag, drop, and resize events</li>
-          <li>Click an event to delete it</li>
-        </ul>
-      </div>
       <div className='demo-app-sidebar-section'>
         <label>
           <input
