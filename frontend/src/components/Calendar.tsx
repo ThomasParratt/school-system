7bfb05da88f24bcd4d5fc466729f5bde3846d599
ThomasParratt <thomasparratt@googlemail.com>
2026-06-12 12:14:07 +0300

    fullcalendar added

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -1,7 +1,14 @@
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+
 export default function Calendar() {
   return (
     <div>
-        <h1>My calendar</h1>
+      <h1 className="text-xl font-bold">Calendar</h1>
+      <FullCalendar
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridMonth"
+      />
     </div>
-  );
+  )
 }
