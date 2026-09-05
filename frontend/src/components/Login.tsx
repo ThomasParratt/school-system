e83f85e3ce82ffa779541e6907e9098cf2e9af07
ThomasParratt <thomasparratt@googlemail.com>
2026-09-05 21:58:47 +0300

    fixed backend dockerfile prod stage was missing ssl

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -46,7 +46,7 @@ function Login() {
             {!user ? (
                 <div className="max-w-md mx-auto bg-white p-6 rounded-lg border border-gray-300">
                     <h1 className="text-3xl font-bold mb-8 text-center text-gray-800">
-                        Frog Management System
+                        Newt Management System
                     </h1>
                     <h2 className="text-2xl font-semibold mb-4 text-gray-700">
                         Login
